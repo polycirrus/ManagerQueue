354b2f7230ab32b331b2a1808cd5aa3bae8bd1db
Aliaksandr Zhytnitski <polycirrus@gmail.com>
2017-05-31 12:42:14 +0300

    Add cover page, task page, economic task

--- BSUIR.ManagerQueue/TestAspService/Controllers/QueueController.cs
+++ BSUIR.ManagerQueue/TestAspService/Controllers/QueueController.cs
@@ -15,6 +15,7 @@ namespace TestAspService.Controllers
     using BSUIR.ManagerQueue.Data;
     using BSUIR.ManagerQueue.Data.Model;
     using BSUIR.ManagerQueue.Infrastructure;
+    using System.Web.Http.Description;
 
     [Authorize]
     public class QueueController : ApiController
@@ -66,6 +67,7 @@ namespace TestAspService.Controllers
 
         // GET: api/Queue/5
         [Authorize(Roles = RoleNames.Manager + ", " + RoleNames.Vice + ", " + RoleNames.Secretary + ", " + RoleNames.Administrator)]
+        [ResponseType(typeof(IEnumerable<QueueItem>))]
         public async Task<IHttpActionResult> Get(int id)
         {
             var userId = User.Identity.GetUserId<int>();
@@ -73,6 +75,57 @@ namespace TestAspService.Controllers
                 return Unauthorized();
 
             return Ok(await dbContext.Queue.Where(queueItem => queueItem.ManagerId == id).ToArrayAsync());
+        }
+
+        // GET: api/Queue/GetEntries
+        [HttpGet]
+        [ResponseType(typeof(IEnumerable<QueueItem>))]
+        public async Task<IHttpActionResult> GetEntries()
+        {
+            var userId = User.Identity.GetUserId<int>();
+            return await GetEntries(userId);
+        }
+
+        // GET: api/Queue/GetEntries/5
+        [HttpGet]
+        [Authorize(Roles = RoleNames.Administrator)]
+        [ResponseType(typeof(IEnumerable<QueueItem>))]
+        public async Task<IHttpActionResult> GetEntries(int userId)
+        {
+            var user = await UserManager.FindByIdAsync(userId);
+            if (user == null)
+                return BadRequest();
+
+            var entries = user.ForeignQueueEntries;
+            return Ok(entries);
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> PostEntry(int queueId, int entrantId)
+        {
+            var currentUserId = User.Identity.GetUserId<int>();
+            if (entrantId != currentUserId && !await UserManager.IsInRoleAsync(currentUserId, RoleNames.Administrator))
+                return Unauthorized();
+
+            var manager = await UserManager.FindByIdAsync(queueId);
+            if (manager == null)
+                return BadRequest();
+
+            var queue = manager.OwnQueueEntries;
+            if (queue.Any(queueItem => queueItem.EmployeeId == entrantId))
+                return BadRequest();
+
+            var newEntryOrder = queue.Max(queueItem => queueItem.Order) + 1;
+            var newEntry = new QueueItem()
+            {
+                ManagerId = queueId,
+                EmployeeId = entrantId,
+                Order = newEntryOrder
+            };
+
+            dbContext.Queue.Add(newEntry);
+            await dbContext.SaveChangesAsync();
+            return Ok();
         }
 
         // PUT: api/Queue/5
@@ -89,6 +142,8 @@ namespace TestAspService.Controllers
             var originalQueue = await dbContext.Queue.Where(queueItem => queueItem.ManagerId == queueId).ToArrayAsync();
             if (originalQueue.Length != queueItems.Count())
                 return BadRequest();
+
+            throw new NotImplementedException();
         }
 
         private async Task<bool> UserHasAccessToQueue(int userId, int queueId)
