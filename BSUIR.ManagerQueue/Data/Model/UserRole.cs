3095f56a69b9095d4d6aa1e06f787ccaf3d10365
Aliaksandr Zhytnitski <polycirrus@gmail.com>
2017-06-07 12:32:37 +0300

    Remove Role navigation property from UserRole to fix user fetch

--- BSUIR.ManagerQueue/Data/Model/UserRole.cs
+++ BSUIR.ManagerQueue/Data/Model/UserRole.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections.Generic;
-
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace BSUIR.ManagerQueue.Data.Model
 {
     public class UserRole : IdentityUserRole<int>
     {
-        public virtual Role Role { get; set; }
     }
 }
